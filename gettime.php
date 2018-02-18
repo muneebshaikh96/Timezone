5ad8da34ff7da681e04a95d10f0720dc46bbfb56
Muhammad Muneeb Shaikh <36607277+muneebshaikh96@users.noreply.github.com>
2018-02-19 02:10:24 +0500

    Update gettime.php

--- gettime.php
+++ gettime.php
@@ -11,7 +11,6 @@ curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 $responseJson = curl_exec($ch);
 curl_close($ch);
- //echo $responseJson;
 $response = json_decode($responseJson);
 
 $timeZoneId = $response->timeZoneId; 
