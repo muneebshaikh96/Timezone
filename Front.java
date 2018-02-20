1c22abf6ec78da212acfcfbfdfddccc2541c9be1
Muhammad Muneeb Shaikh <36607277+muneebshaikh96@users.noreply.github.com>
2018-02-21 00:14:15 +0500

    Update Front.java

--- Front.java
+++ Front.java
@@ -17,18 +17,9 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
-import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.TimeZone;
 
 public class Front extends AppCompatActivity {
